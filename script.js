395cee16e6be62305dfa234265d8957b8dec449d
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-12-01 19:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function volume_sphere() {
-    //Write your code here
+     const radiusInput = document.getElementById("radius");
+    const radiusValue = parseFloat(radiusInput.value);
+
+    // Check if the entered value is a valid number
+    if (isNaN(radiusValue) || radiusValue <= 0) {
+        alert("Please enter a valid positive number for the radius.");
+        return;
+    }
+
+    // Calculate the volume of the sphere using the formula V = (4/3) * π * r^3
+    const volume = (4 / 3) * Math.PI * Math.pow(radiusValue, 3);
+
+    // Display the calculated volume in the output field
+    const volumeOutput = document.getElementById("volume");
+    volumeOutput.value = volume.toFixed(2); // Round to 2 decimal places
+}
   
 } 
 
